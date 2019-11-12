5cafd23660cf380c9339d36eafc6915117d5f3ee
eltiko <elticco@hotmail.com>
2019-11-11 22:45:33 -0800

    start 11

--- code-challenges/chaining-methods/challenges-10.test.js
+++ code-challenges/chaining-methods/challenges-10.test.js
@@ -37,6 +37,14 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 ------------------------------------------------------------------------------------------------ */
 
 const totalSum = (input) => {
+
+
+  let total = 0;
+  input.forEach( (array) => {
+    return array.reduce( (tot,num) => total += num , 0);
+  });
+  return total;
+
   // Solution code here...
 };
 
